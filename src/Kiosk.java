ebcadd975fde4bec6e087ec6a029bc8f2680dd36
박성현 <110206842+shPark95@users.noreply.github.com>
2025-04-24 20:34:24 +0900

    Add: Menu class

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -3,49 +3,80 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
-    private List<MenuItem> menuItems;
+    private List<Menu> menuList;
 
-    public Kiosk(List<MenuItem> menuItems) {
-        this.menuItems = menuItems;
+    public Kiosk(List<Menu> menuList) {
+        this.menuList = menuList;
     }
 
     public void start() {
         Scanner sc = new Scanner(System.in);
-        boolean flag = true;
-        while (flag) {
-            showHambergMenu();
-            flag = showSelectedMenu(sc.next());
+
+        while (true) {
+            int input1 = showMainMenu(sc);
+            if (input1 == 0) {
+                break;
+            } else {
+                if (!checkValidMainMenu(input1)) {
+                    continue;
+                }
+            }
+
+            int input2 = showSubMenu(sc, input1);
+            if (input2 == 0) continue;
+
+            boolean continueFlag = showSelectedMenu(input1 - 1, input2 - 1);
+            if (!continueFlag) break;
         }
+
     }
 
-    public boolean showSelectedMenu(String input) {
-        try {
-            int index = Integer.parseInt(input);
-            if (index == 0) {
-                System.out.println("프로그램을 종료");
-                return false;
-            } else if (index < menuItems.size()) {
-                System.out.println("==============선택한 메뉴===============");
-                System.out.printf("이름 : %s\n", menuItems.get(index).getName());
-                System.out.printf("가격 : %.1f\n", menuItems.get(index).getPrice());
-                System.out.printf("설명 : %s\n", menuItems.get(index).getDescription());
-                System.out.println();
-                return true;
-            } else {
-                System.out.println("잘못된 숫자범위입니다.");
-                return true;
-            }
-        } catch (Exception e) {
-            System.out.println("잘못된 입력입니다.");
+    private boolean checkValidMainMenu(int input) {
+        if (input < 0 || input > menuList.size()) {
+            return false;
+        } else {
             return true;
         }
     }
 
-    public void showHambergMenu() {
-        System.out.println("[ SHAKESHACK MENU ]");
-        for (MenuItem menuItem : menuItems) {
-            System.out.printf("%d. %s | W %.1f | %s\n", menuItem.getIndex(), menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
-        }
-        System.out.println("0. 종료      | 종료");
+    private int showMainMenu(Scanner sc) {
+        showSHAKESHACKMainMenu();
+        return inputnumber(sc.next());
     }
+
+    private int showSubMenu(Scanner sc, int input1) {
+        menuList.get(input1 - 1).showMenu();
+        return inputnumber(sc.next());
+    }
+
+    public int inputnumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 입력입니다.");
+            return -1;
+        }
+    }
+
+    public void showSHAKESHACKMainMenu() {
+        System.out.println("[ MAIN MENU ]");
+        for (Menu menu : menuList) {
+            System.out.printf("%d. %s\n", menu.getIndex(), menu.getCategory());
+        }
+    }
+
+    public boolean showSelectedMenu(int input1, int input2) {
+        if (input2 < menuList.get(input1).getMenuItems().size()) {
+            System.out.println("==============선택한 메뉴===============");
+            System.out.printf("이름 : %s\n", menuList.get(input1).getMenuItems().get(input2).getName());
+            System.out.printf("가격 : %.1f\n", menuList.get(input1).getMenuItems().get(input2).getPrice());
+            System.out.printf("설명 : %s\n", menuList.get(input1).getMenuItems().get(input2).getDescription());
+            System.out.println();
+            return true;
+        } else {
+            System.out.println("잘못된 숫자범위입니다.");
+            return true;
+        }
+    }
+
 }
